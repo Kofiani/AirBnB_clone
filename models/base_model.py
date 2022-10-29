20911cbd624b5683db9960461c0a7eaeccc455fe
Kofiani <samuelannie.k@gmail.com>
2022-10-29 21:49:36 +0100

    updated base_model, engine/file_storage, __init__

--- models/base_model.py
+++ models/base_model.py
@@ -1,17 +1,23 @@
 #!/usr/bin/python3
-'''This is the base model for the airbnb clone project'''
+"""This is the base model for the airbnb clone project."""
 
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    '''The Base Model from which all all objects
-        are instantiated'''
+    """The Base Model from which all all objects are instantiated."""
+
     created_at = datetime.today()
     updated_at = datetime.today()
 
     def __init__(self, *args, **kwargs):
+        """Args: use kwargs.
+
+        Pass dictionary to create a new instance/object.
+        If parameter is passed create a new instance/object.
+        storage.new(self) passes the address of the new instance to storage.
+        """
         from models import storage
         if not kwargs:
             self.id = id = str(uuid.uuid4())
@@ -27,11 +33,22 @@ class BaseModel:
                         setattr(self, key, kwargs[key])
 
     def save(self):
+        """Save new instance to file.
+
+        Update time if instance is updated.
+        storage.save() saves the instance to storage.
+        """
         from models import storage
         self.updated_at = datetime.now()
         storage.save()
 
     def to_dict(self):
+        """Create a dictionary for JSON.
+
+        Convert the data dictionary to be serialized with JSON.
+        Create a new key called __class__.
+        The new key will be used to create a new object deserializing with JSON.
+        """
         self.created_at = self.created_at.isoformat()
         self.updated_at = self.updated_at.isoformat()
         add_class_key = dict(__class__=self.__class__.__name__)
@@ -39,5 +56,6 @@ class BaseModel:
         return add_class_key
 
     def __str__(self):
+        """Create a string representation of the object."""
         return '[' + self.__class__.__name__ + ']' +\
                 '(' + self.id + ')' + repr(self.__dict__)
